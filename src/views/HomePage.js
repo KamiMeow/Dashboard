8238b192341ef2b71403af6bfbca89ce28151d1b
Дима Бурмистров <supamem74@gmail.com>
2020-09-07 16:11:27 +0300

    feat(views): add pages-template
    Two new pages:
    - Login;
    - Register.

---
+++ src/views/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HomePage() {
+	return (
+		<div style={{
+			background: "purple",
+			color: "white",
+		}}>
+			<h2>
+				Home page
+			</h2>
+		</div>
+	);
+};
+
+export default HomePage;
---
+++ src/views/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoginPage () {
+	return (
+		<div style={{
+			background: "green",
+			color: "white",
+		}}>
+			Login page fsdfghsdkhf
+		</div>
+	);
+};
+
+export default LoginPage;
