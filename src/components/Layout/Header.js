16b5ede6d38de590afc7000f84e34fa7c59d9fb8
Дима Бурмистров <supamem74@gmail.com>
2020-09-07 16:22:46 +0300

    feat(router): add links to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import routes from '@/router';
 
 function Header(props) {
+
+	console.log(routes);
+	const links = routes.map( (route, i) => (
+		
+		<Link 
+			key={route.route + i} 
+			to={route.route} 
+			>
+			<span> {route.title} </span>
+		</Link>
+	));
+
   return (
     <header>
       <h2>
 				Header
 			</h2>
-			<Link to="/">
-				<span> Home </span>
-			</Link>
-			<Link to="/about">
-				<span> About </span>
-			</Link>
+			{links}
     </header>
   );
 };
