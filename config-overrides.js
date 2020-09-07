700b4b65fcb946b9bc417e3d9c0bb3687b904824
Дима Бурмистров <supamem74@gmail.com>
2020-09-07 16:16:42 +0300

    refactor(project configuration): add new aliases

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 module.exports = override(
   addWebpackAlias({
-    ['@styles']: path.resolve(__dirname, './src/styles'),
+		['@styles']: path.resolve(__dirname, './src/styles'),
+		['@assets']: path.resolve(__dirname, './src/assets'),
+		['@views']: path.resolve(__dirname, './src/views'),
     ['@cmp']: path.resolve(__dirname, './src/components'),
-    ['@']: path.resolve(__dirname, './src'),
+		['@']: path.resolve(__dirname, './src'),
   }),
 );
